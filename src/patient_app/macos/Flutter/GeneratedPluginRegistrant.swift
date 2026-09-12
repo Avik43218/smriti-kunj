7952389190e5df411917e6b439d63301d694fea2
Avik43218 <roychoudhuryavik005@gmail.com>
2026-09-13 04:55:07 +0530

    fix: Flutter mobile app fixed

--- src/patient_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/patient_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_tts
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
